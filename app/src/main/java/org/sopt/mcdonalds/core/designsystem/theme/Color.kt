410e4ed32b4e9f00fed0abe0182f0d403aa0bffd
Jong <jm991014@gmail.com>
2025-05-11 17:14:42 +0900

    [add/#5] color, typography 설정 및 theme 반영

--- app/src/main/java/org/sopt/mcdonalds/core/designsystem/theme/Color.kt
+++ app/src/main/java/org/sopt/mcdonalds/core/designsystem/theme/Color.kt
@@ -1,11 +1,179 @@
 package org.sopt.mcdonalds.core.designsystem.theme
 
+import androidx.compose.runtime.Stable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Main
+val lightYellow = Color(0xFFFFF2CF)
+val yellow = Color(0xFFFFBC0D)
+val darkYellow = Color(0xFFE5A90B)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Point
+val blue = Color(0xFF307AC4)
+val lightRed = Color(0xFFFFD0D0)
+val red = Color(0xFFFF195E)
+val orange = Color(0xFFF08849)
+
+// Static
+val black = Color(0xFF000000)
+val white = Color(0xFFFFFFFF)
+
+// Gray Scale
+val gray800 = Color(0xFF292929)
+val gray750 = Color(0xFF3F3F3F)
+val gray700 = Color(0xFF646464)
+val gray600 = Color(0xFF787878)
+val gray500 = Color(0xFFA2A2A2)
+val gray400 = Color(0xFFB2B2B2)
+val gray300 = Color(0xFFD9D9D9)
+val gray200 = Color(0xFFE3E3E3)
+val gray100 = Color(0xFFF3F3F3)
+
+@Stable
+class McDonaldsColors(
+    lightYellow: Color,
+    yellow: Color,
+    darkYellow: Color,
+    blue: Color,
+    lightRed: Color,
+    red: Color,
+    orange: Color,
+    black: Color,
+    white: Color,
+    gray800: Color,
+    gray750: Color,
+    gray700: Color,
+    gray600: Color,
+    gray500: Color,
+    gray400: Color,
+    gray300: Color,
+    gray200: Color,
+    gray100: Color,
+    isLight: Boolean,
+) {
+    var lightYellow by mutableStateOf(lightYellow)
+        private set
+    var yellow by mutableStateOf(yellow)
+        private set
+    var darkYellow by mutableStateOf(darkYellow)
+        private set
+    var blue by mutableStateOf(blue)
+        private set
+    var lightRed by mutableStateOf(lightRed)
+        private set
+    var red by mutableStateOf(red)
+        private set
+    var orange by mutableStateOf(orange)
+        private set
+    var black by mutableStateOf(black)
+        private set
+    var white by mutableStateOf(white)
+        private set
+    var gray800 by mutableStateOf(gray800)
+        private set
+    var gray750 by mutableStateOf(gray750)
+        private set
+    var gray700 by mutableStateOf(gray700)
+        private set
+    var gray600 by mutableStateOf(gray600)
+        private set
+    var gray500 by mutableStateOf(gray500)
+        private set
+    var gray400 by mutableStateOf(gray400)
+        private set
+    var gray300 by mutableStateOf(gray300)
+        private set
+    var gray200 by mutableStateOf(gray200)
+        private set
+    var gray100 by mutableStateOf(gray100)
+        private set
+    var isLight by mutableStateOf(isLight)
+
+    fun copy(): McDonaldsColors = McDonaldsColors(
+        lightYellow,
+        yellow,
+        darkYellow,
+        blue,
+        lightRed,
+        red,
+        orange,
+        black,
+        white,
+        gray800,
+        gray750,
+        gray700,
+        gray600,
+        gray500,
+        gray400,
+        gray300,
+        gray200,
+        gray100,
+        isLight,
+    )
+
+    fun update(other: McDonaldsColors) {
+        lightYellow = other.lightYellow
+        yellow = other.yellow
+        darkYellow = other.darkYellow
+        blue = other.blue
+        lightRed = other.lightRed
+        red = other.red
+        orange = other.orange
+        black = other.black
+        white = other.white
+        gray800 = other.gray800
+        gray750 = other.gray750
+        gray700 = other.gray700
+        gray600 = other.gray600
+        gray500 = other.gray500
+        gray400 = other.gray400
+        gray300 = other.gray300
+        gray200 = other.gray200
+        gray100 = other.gray100
+        isLight = other.isLight
+    }
+}
+
+fun McDonaldsLightColors(
+    LightYellow: Color = lightYellow,
+    Yellow: Color = yellow,
+    DarkYellow: Color = darkYellow,
+    Blue: Color = blue,
+    LightRed: Color = lightRed,
+    Red: Color = red,
+    Orange: Color = orange,
+    Black: Color = black,
+    White: Color = white,
+    Gray800: Color = gray800,
+    Gray750: Color = gray750,
+    Gray700: Color = gray700,
+    Gray600: Color = gray600,
+    Gray500: Color = gray500,
+    Gray400: Color = gray400,
+    Gray300: Color = gray300,
+    Gray200: Color = gray200,
+    Gray100: Color = gray100,
+) = McDonaldsColors(
+    LightYellow,
+    Yellow,
+    DarkYellow,
+    Blue,
+    LightRed,
+    Red,
+    Orange,
+    Black,
+    White,
+    Gray800,
+    Gray750,
+    Gray700,
+    Gray600,
+    Gray500,
+    Gray400,
+    Gray300,
+    Gray200,
+    Gray100,
+    isLight = true,
+)
--- app/src/main/java/org/sopt/mcdonalds/core/designsystem/theme/Theme.kt
+++ app/src/main/java/org/sopt/mcdonalds/core/designsystem/theme/Theme.kt
@@ -1,57 +1,58 @@
 package org.sopt.mcdonalds.core.designsystem.theme
 
-import android.os.Build
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.staticCompositionLocalOf
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
+private val LocalMcDonaldsColors = staticCompositionLocalOf<McDonaldsColors> {
+    error("No McDonaldsColors provided")
+}
 
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+private val LocalMcDonaldsTypography = staticCompositionLocalOf<McDonaldsTypography> {
+    error("No McDonaldsTypography provided")
+}
 
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
-)
+object McDonaldsTheme {
+    val colors: McDonaldsColors
+        @Composable
+        @ReadOnlyComposable
+        get() = LocalMcDonaldsColors.current
+    val typography: McDonaldsTypography
+        @Composable
+        @ReadOnlyComposable
+        get() = LocalMcDonaldsTypography.current
+}
+
+@Composable
+fun ProvideMcDonaldsColorsAndTypography(
+    colors: McDonaldsColors,
+    typography: McDonaldsTypography,
+    content: @Composable () -> Unit,
+) {
+    val provideColors = remember { colors.copy() }.apply { update(colors) }
+    CompositionLocalProvider(
+        LocalMcDonaldsColors provides provideColors,
+        LocalMcDonaldsTypography provides typography,
+        content = content,
+    )
+}
 
 @Composable
 fun MCDONALDSTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
-    content: @Composable () -> Unit
+    content: @Composable () -> Unit,
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
+    val colors = McDonaldsLightColors()
+    val typography = McDonaldsTypography()
 
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+    ProvideMcDonaldsColorsAndTypography(
+        colors = colors,
+        typography = typography,
+    ) {
+        MaterialTheme(
+            content = content,
+        )
     }
-
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
-        content = content
-    )
 }
