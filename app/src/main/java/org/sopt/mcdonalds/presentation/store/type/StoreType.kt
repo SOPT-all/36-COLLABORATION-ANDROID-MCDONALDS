f6f9ee414466f6b9270bbc4e2ec8c61ae819522e
Jong <jm991014@gmail.com>
2025-05-15 23:49:24 +0900

    [add/#14] StoreType 추가

---
+++ app/src/main/java/org/sopt/mcdonalds/presentation/store/type/StoreType.kt
@@ -0,0 +1,14 @@
+package org.sopt.mcdonalds.presentation.store.type
+
+import androidx.annotation.StringRes
+import org.sopt.mcdonalds.R.string.mc_drive
+import org.sopt.mcdonalds.R.string.open_24_hours
+import org.sopt.mcdonalds.R.string.garage
+
+enum class StoreType(
+    @StringRes val title: Int,
+) {
+    MCDRIVE(title = mc_drive),
+    OPEN_24_HOURS(title = open_24_hours),
+    GARAGE(title = garage);
+}
