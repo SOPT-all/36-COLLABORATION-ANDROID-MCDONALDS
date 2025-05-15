080ab3e0477814039f3310feb1ec1c2f841b080c
sosongha3 <sosongha3@ajou.ac.kr>
2025-05-15 17:30:33 +0900

    [ui/#10] HistoryTimeBar 컴포넌트 추가

---
+++ app/src/main/java/org/sopt/mcdonalds/presentation/history/component/HistoryTimeBar.kt
@@ -0,0 +1,74 @@
+package org.sopt.mcdonalds.presentation.history.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import org.sopt.mcdonalds.R
+import org.sopt.mcdonalds.core.designsystem.theme.MCDONALDSTheme
+import org.sopt.mcdonalds.core.designsystem.theme.McDonaldsTheme
+
+@Composable
+fun HistoryTimeBar(
+    endTime: LocalDateTime,
+    modifier: Modifier = Modifier,
+) {
+    val formatter = DateTimeFormatter.ofPattern("HH:mm")
+    Column {
+        Spacer(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(1.dp)
+                .background(color = McDonaldsTheme.colors.gray200),
+        )
+        Row (
+            modifier = modifier
+                .fillMaxWidth()
+                .background(color = McDonaldsTheme.colors.gray100)
+                .padding(start = 24.dp, end = 24.dp, top = 7.dp, bottom = 7.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(15.dp),
+        ) {
+            Icon(
+                imageVector = ImageVector.vectorResource(R.drawable.ic_warning_20),
+                contentDescription = null,
+                tint = Color. Unspecified,
+            )
+            Text(
+                text = stringResource(R.string.history_time_notice, endTime.format(formatter)),
+                style = McDonaldsTheme.typography.body14r,
+                color = McDonaldsTheme.colors.black,
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun HistoryTimeBarPreview(){
+    MCDONALDSTheme {
+        Column {
+            HistoryTimeBar(
+                endTime = LocalDateTime.now(),
+                modifier = Modifier,
+            )
+        }
+    }
+}
