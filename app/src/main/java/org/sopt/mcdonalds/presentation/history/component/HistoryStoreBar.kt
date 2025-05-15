55e5c42babc2d5f3af57907e8540e0391739626f
sosongha3 <sosongha3@ajou.ac.kr>
2025-05-15 17:41:10 +0900

    [ui/#10] HistoryStoreBar 컴포넌트 추가

---
+++ app/src/main/java/org/sopt/mcdonalds/presentation/history/component/HistoryStoreBar.kt
@@ -0,0 +1,85 @@
+
+package org.sopt.mcdonalds.presentation.history.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import org.sopt.mcdonalds.R
+import org.sopt.mcdonalds.core.designsystem.theme.MCDONALDSTheme
+import org.sopt.mcdonalds.core.designsystem.theme.McDonaldsTheme
+
+@Composable
+fun HistoryStoreBar(
+    store: String,
+    onStoreChangeClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Column {
+        Row (
+            modifier = modifier
+                .fillMaxWidth()
+                .background(color = McDonaldsTheme.colors.white)
+                .padding(start = 20.dp, end = 20.dp, top = 12.dp, bottom = 12.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween,
+        ) {
+            Row (
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(10.dp),
+            ) {
+                Icon(
+                    imageVector = ImageVector.vectorResource(R.drawable.ic_mcdonalds_location),
+                    contentDescription = null,
+                    tint = Color.Unspecified,
+                )
+                Text(
+                    text = store,
+                    style = McDonaldsTheme.typography.body14b,
+                    color = McDonaldsTheme.colors.black,
+                )
+            }
+            HistoryStoreChangeButton(
+                text = stringResource(R.string.history_store_change),
+                onClick = onStoreChangeClick,
+            )
+        }
+        Spacer(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(1.dp)
+                .background(color = McDonaldsTheme.colors.gray300),
+        )
+    }
+}
+
+@Preview
+@Composable
+fun HistoryStoreBarPreview(){
+    MCDONALDSTheme {
+        Column {
+            HistoryStoreBar(
+                store = "양평SK DT",
+                onStoreChangeClick = {},
+                modifier = Modifier,
+            )
+        }
+    }
+}
