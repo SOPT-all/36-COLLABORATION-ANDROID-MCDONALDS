2c761e8b2c12b945913afcaa791d5d3a0ba0d527
ImHyungsuk <limhs8630@gmail.com>
2025-05-16 17:00:54 +0900

    [add/#17] 단품/세트 버튼 프리뷰 수정

--- app/src/main/java/org/sopt/mcdonalds/presentation/order/component/OrderSetSelectButton.kt
+++ app/src/main/java/org/sopt/mcdonalds/presentation/order/component/OrderSetSelectButton.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
@@ -92,13 +93,16 @@ fun OrderSetSelectButton(
 @Composable
 fun OrderSetSelectButtonPreview() {
     Column {
-        OrderSetSelectButton("단품", 10000, false, R.drawable.img_burger_single)
-        Spacer(modifier = Modifier.height(2.dp))
-        OrderSetSelectButton("단품", 10000, true, R.drawable.img_burger_single)
-        Spacer(modifier = Modifier.height(2.dp))
-        OrderSetSelectButton("세트", 10000, false, R.drawable.img_burger_set)
-        Spacer(modifier = Modifier.height(2.dp))
-        OrderSetSelectButton("세트", 10000, true, R.drawable.img_burger_set)
-        Spacer(modifier = Modifier.height(2.dp))
+        Row {
+            OrderSetSelectButton("단품", 10000, false, R.drawable.img_burger_single)
+            Spacer(modifier = Modifier.width(8.dp))
+            OrderSetSelectButton("단품", 10000, true, R.drawable.img_burger_single)
+        }
+        Spacer(modifier = Modifier.height(8.dp))
+        Row {
+            OrderSetSelectButton("세트", 10000, false, R.drawable.img_burger_set)
+            Spacer(modifier = Modifier.width(8.dp))
+            OrderSetSelectButton("세트", 10000, true, R.drawable.img_burger_set)
+        }
     }
 }
