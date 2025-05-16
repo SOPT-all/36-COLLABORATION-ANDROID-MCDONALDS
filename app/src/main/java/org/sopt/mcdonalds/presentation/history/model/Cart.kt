fe45c46104d34e81a656f2ca2948644cde314df6
sosongha3 <sosongha3@ajou.ac.kr>
2025-05-16 21:26:55 +0900

    [add/#20] RecentBurger 데이터 클래스 추가

--- app/src/main/java/org/sopt/mcdonalds/presentation/history/model/Cart.kt
+++ app/src/main/java/org/sopt/mcdonalds/presentation/history/model/Cart.kt
@@ -3,6 +3,8 @@ package org.sopt.mcdonalds.presentation.history.model
 data class Cart(
     val cartId: Long,
     val amount: Int,
+    val price: Int,
     val menuName: String,
-    val imageUrl: String
+    val imageUrl: String,
+    val isSet: Boolean
 )
---
+++ app/src/main/java/org/sopt/mcdonalds/presentation/history/model/RecentBurger.kt
@@ -0,0 +1,8 @@
+package org.sopt.mcdonalds.presentation.history.model
+
+data class RecentBurger(
+    val menuId: Long,
+    val menuName: String,
+    val menuImage: String,
+    val menuPrice: String
+)
