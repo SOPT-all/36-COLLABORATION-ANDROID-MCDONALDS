3ff5c2ad30f7e242fc109d8d11370c35ee583395
Jong <jm991014@gmail.com>
2025-05-20 01:06:47 +0900

    [add/#34] menu detail 모델 생성

---
+++ app/src/main/java/org/sopt/mcdonalds/domain/menu/model/MenuDetail.kt
@@ -0,0 +1,10 @@
+package org.sopt.mcdonalds.domain.menu.model
+
+data class MenuDetail(
+    val id: Long,
+    val name: String,
+    val singleImg: String,
+    val singlePrice: String,
+    val setImg: String,
+    val setPrice: String,
+)
