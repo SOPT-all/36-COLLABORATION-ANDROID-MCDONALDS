ab3788918b3255f3173f2284cc79d8b9b9f7e10d
Jong <jm991014@gmail.com>
2025-05-19 10:53:04 +0900

    [feat/#26] 메뉴 네비게이션 추가

---
+++ app/src/main/java/org/sopt/mcdonalds/presentation/menu/navigation/MenuListNavigation.kt
@@ -0,0 +1,28 @@
+package org.sopt.mcdonalds.presentation.menu.navigation
+
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavOptions
+import androidx.navigation.compose.composable
+import kotlinx.serialization.Serializable
+import org.sopt.mcdonalds.core.common.navigation.Route
+import org.sopt.mcdonalds.presentation.menu.MenuListRoute
+
+fun NavController.navigateToMenuList(navOptions: NavOptions? = null) =
+    navigate(MenuList, navOptions)
+
+fun NavGraphBuilder.menuListGraph(
+    onNavigateToOrder: (Long) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    composable<MenuList> {
+        MenuListRoute(
+            onNavigateToOrder = onNavigateToOrder,
+            modifier = modifier
+        )
+    }
+}
+
+@Serializable
+data object MenuList : Route
