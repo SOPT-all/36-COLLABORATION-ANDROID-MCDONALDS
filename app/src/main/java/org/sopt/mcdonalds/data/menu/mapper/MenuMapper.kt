8a84c96c4d61337739c22acbf56302f975f7e5a3
Jong <jm991014@gmail.com>
2025-05-20 01:07:13 +0900

    [feat/#34] 메뉴 mapper 및 repository 구현체 구현

---
+++ app/src/main/java/org/sopt/mcdonalds/data/menu/mapper/MenuMapper.kt
@@ -0,0 +1,24 @@
+package org.sopt.mcdonalds.data.menu.mapper
+
+import org.sopt.mcdonalds.data.menu.dto.MenuDetailResponse
+import org.sopt.mcdonalds.data.menu.dto.MenuListResponse
+import org.sopt.mcdonalds.domain.menu.model.Menu
+import org.sopt.mcdonalds.domain.menu.model.MenuDetail
+
+fun MenuListResponse.toDomain() = this.menus.map {
+    Menu(
+        id = it.id,
+        name = it.name,
+        imageUrl = it.img,
+        price = it.price,
+    )
+}
+
+fun MenuDetailResponse.toDomain() = MenuDetail(
+    id = this.id,
+    name = this.name,
+    singleImg = this.singleImg,
+    singlePrice = this.singlePrice,
+    setImg = this.setImg,
+    setPrice = this.setPrice,
+)
---
+++ app/src/main/java/org/sopt/mcdonalds/data/menu/repository/MenuRepositoryImpl.kt
@@ -0,0 +1,24 @@
+package org.sopt.mcdonalds.data.menu.repository
+
+import org.sopt.mcdonalds.data.menu.datasource.MenuDataSource
+import org.sopt.mcdonalds.data.menu.mapper.toDomain
+import org.sopt.mcdonalds.domain.menu.model.Menu
+import org.sopt.mcdonalds.domain.menu.model.MenuDetail
+import org.sopt.mcdonalds.domain.menu.repository.MenuRepository
+import javax.inject.Inject
+
+class MenuRepositoryImpl @Inject constructor(
+    private val menuDataSource: MenuDataSource,
+) : MenuRepository {
+    override suspend fun getMenus(): Result<List<Menu>> = runCatching {
+        menuDataSource.getMenus()
+    }.mapCatching {
+        it.toDomain()
+    }
+
+    override suspend fun getMenuDetail(menuId: Long): Result<MenuDetail> = runCatching {
+        menuDataSource.getMenuDetail(menuId)
+    }.mapCatching {
+        it.toDomain()
+    }
+}
