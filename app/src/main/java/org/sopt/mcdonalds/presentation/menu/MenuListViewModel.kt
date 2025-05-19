843b9f69f1ae909d7a457af203d5023322f4b1eb
Jong <jm991014@gmail.com>
2025-05-20 01:08:38 +0900

    [feat/#34] 뷰모델에 유스케이스 연동

--- app/src/main/java/org/sopt/mcdonalds/presentation/menu/MenuListViewModel.kt
+++ app/src/main/java/org/sopt/mcdonalds/presentation/menu/MenuListViewModel.kt
@@ -1,29 +1,38 @@
 package org.sopt.mcdonalds.presentation.menu
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import javax.inject.Inject
-import kotlinx.collections.immutable.toImmutableList
+import kotlinx.collections.immutable.toPersistentList
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
-import org.sopt.mcdonalds.presentation.menu.model.Menu
+import kotlinx.coroutines.launch
+import org.sopt.mcdonalds.domain.menu.model.Menu
+import org.sopt.mcdonalds.domain.menu.usecase.GetMenusUseCase
 import org.sopt.mcdonalds.presentation.menu.state.MenuListContract.MenuListState
+import javax.inject.Inject
 
 @HiltViewModel
 class MenuListViewModel @Inject constructor(
-    // TODO: API Connection
+    private val getMenusUseCase: GetMenusUseCase,
 ) : ViewModel() {
     private val _uiState = MutableStateFlow(MenuListState())
     val uiState = _uiState.asStateFlow()
 
     init {
-        // TODO: API call
+        viewModelScope.launch {
+            getMenusUseCase().onSuccess {
+                updateMenuList(it)
+            }.onFailure {
+                // TODO: API Called Failed
+            }
+        }
     }
 
     private fun updateMenuList(menuList: List<Menu>) {
         _uiState.update {
-            it.copy(menuList = menuList.toImmutableList())
+            it.copy(menuList = menuList.toPersistentList())
         }
     }
 }
