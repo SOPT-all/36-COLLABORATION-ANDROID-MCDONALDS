eb0c83a288bd2b6f0035f6fdb732d066c9a29c25
Jong <jm991014@gmail.com>
2025-05-19 10:27:10 +0900

    [feat/#25] 메인 화면 네비게이션 추가

---
+++ app/src/main/java/org/sopt/mcdonalds/presentation/main/MainScreen.kt
@@ -0,0 +1,46 @@
+package org.sopt.mcdonalds.presentation.main
+
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import org.sopt.mcdonalds.presentation.store.navigation.Store
+import org.sopt.mcdonalds.presentation.store.navigation.storeGraph
+
+@Composable
+fun MainScreen() {
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+    ) { innerPadding ->
+        MainNavHost(
+            navController = rememberNavController(),
+            modifier = Modifier.padding(innerPadding),
+        )
+    }
+}
+
+@Composable
+private fun MainNavHost(
+    navController: NavHostController,
+    modifier: Modifier = Modifier,
+) {
+    NavHost(
+        enterTransition = { EnterTransition.None },
+        exitTransition = { ExitTransition.None },
+        popEnterTransition = { EnterTransition.None },
+        popExitTransition = { ExitTransition.None },
+        navController = navController,
+        startDestination = Store,
+    ) {
+        storeGraph(
+            onNavigateToMenuList = { /* TODO */ },
+            modifier = modifier,
+        )
+    }
+}
