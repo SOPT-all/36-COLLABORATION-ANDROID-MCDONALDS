b299060bf918dcd6697bc7a17cb465dd548a5109
Jong <jm991014@gmail.com>
2025-05-19 19:54:41 +0900

    [chore/#26] AndroidEntryPoint 어노테이션 추가

--- app/src/main/java/org/sopt/mcdonalds/presentation/main/MainActivity.kt
+++ app/src/main/java/org/sopt/mcdonalds/presentation/main/MainActivity.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import dagger.hilt.android.AndroidEntryPoint
 import org.sopt.mcdonalds.core.designsystem.theme.MCDONALDSTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
