36e074eab2cde08c3db3a71ebf5cab929e237ad1
Jong <jm991014@gmail.com>
2025-05-22 18:45:08 +0900

    [del/#43] 사용하지 않는 model 삭제

--- app/src/main/java/org/sopt/mcdonalds/presentation/history/model/RecentBurger.kt
+++
@@ -1,8 +0,0 @@
-package org.sopt.mcdonalds.presentation.history.model
-
-data class RecentBurger(
-    val menuId: Long,
-    val menuName: String,
-    val menuImage: String,
-    val menuPrice: String
-)
